581d08f4ceafce66929c96009c2efeadbea8f8a0
Kristiyann19 <karamilevkristiyan@gmail.com>
2023-12-24 18:26:02 +0200

    made login page and simplified the code

--- RentACarApplication/src/app/login/components/login.component.ts
+++ RentACarApplication/src/app/login/components/login.component.ts
@@ -5,7 +5,7 @@ import { LoginService } from '../service/login.service';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   user: LoginDto = { userName: '',  password: '' }; 

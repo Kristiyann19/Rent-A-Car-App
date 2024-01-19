a743e5e07692d90d0f1afaef14461c03b9ad2e6e
DESKTOP-14PH1HC\Kristiyan.Karamilev <karamilevkristiyan@gmail.com>
2024-01-19 15:41:46 +0200

    server side validation for Agent using RegEx

---
+++ RentACarApp/Validations/AgentValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace RentACarApp.Validations;
+using FluentValidation;
+using RentACarApp.Dtos;
+
+public class AgentValidator : AbstractValidator<AgentDto>
+{
+    public AgentValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty().Length(2, 30);
+        RuleFor(x => x.LastName).NotEmpty().Length(2, 30);
+        RuleFor(x => x.PhoneNumber).NotEmpty().Matches("^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$");
+    }
+}
